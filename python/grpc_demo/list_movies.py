5809f862a7a573226ff508319886f76af14b11d7
Jochen Van de Voorde <jochen.van.de.voorde@persgroep.net>
2020-12-12 23:57:24 +0100

    Finalize list movies

--- python/grpc_demo/list_movies.py
+++ python/grpc_demo/list_movies.py
@@ -1,37 +1,36 @@
 #! /usr/bin/env python
+"""Iterate through all movies and print info about them"""
 
-import movie_catalogue_pb2
 import sys
+import movie_catalogue_pb2
 
-# Iterates through all movies and prints info about them
+
 def list_movies(movie_catalogue):
-  for movie in movie_catalogue.movies:
-    print("Title:", movie.title)
-    print("Description:", movie.description)
-    print("Production Year:", movie.productionYear) 
+    """Iterate through all movies and print info about them"""
+    for movie in movie_catalogue.movies:
+        print("Title:", movie.title)
+        print("Description:", movie.description)
+        print("Production Year:", movie.productionYear)
+        print("Genre:", movie.genre)
+        print("Duration:", movie.duration)
 
-    for cast_member in movie.castMembers:
-        print("Character:", cast_member.character)
-        print("First Name:", cast_member.firstName)
-        print("Last Name:", cast_member.lastName)
+        for cast_member in movie.castMembers:
+            print("Character:", cast_member.character)
+            print("First Name:", cast_member.firstName)
+            print("Last Name:", cast_member.lastName)
 
-
-    print("Genre:", movie.genre)
-    print("Duration:", movie.duration)
 
 def main():
-
-    # Read the entire movie catalogue from a file and print all the 
-    # information inside
+    """Read the entire movie catalogue from a file and print all the
+       information inside"""
     if len(sys.argv) != 2:
-      print("Usage:", sys.argv[0], "MOVIE_CATALOGUE_FILE")
-      sys.exit(-1)
+        print("Usage:", sys.argv[0], "MOVIE_CATALOGUE_FILE")
+        sys.exit(-1)
 
     movie_catalogue = movie_catalogue_pb2.MovieCatalogue()
 
-    # Read the existing address book.
+    # Read the existing movie catalogue
     with open(sys.argv[1], "rb") as f:
-    movie_catalogue.ParseFromString(f.read())
+        movie_catalogue.ParseFromString(f.read())
 
     list_movies(movie_catalogue)
-
