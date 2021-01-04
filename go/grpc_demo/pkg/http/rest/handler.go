09653fbc366e96e535fc50ce0d76f2556f78ef92
Jochen Van de Voorde <jochen.van.de.voorde@persgroep.net>
2021-01-04 21:25:49 +0100

    Add movie route

--- go/grpc_demo/pkg/http/rest/handler.go
+++ go/grpc_demo/pkg/http/rest/handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/jovv/grpc_demo/go/grpc_demo/pkg/listing"
 	"github.com/julienschmidt/httprouter"
@@ -14,7 +15,7 @@ func Handler(l listing.Service) http.Handler {
 	router := httprouter.New()
 
 	router.GET("/movies", getMovies(l))
-	// router.GET("/movie/:id", getMovie(l))
+	router.GET("/movie/:id", getMovie(l))
 	// router.GET("/movie/:id/castmembers", getMovieCastMembers(l))
 
 	return router
@@ -32,6 +33,32 @@ func getMovies(s listing.Service) func(w http.ResponseWriter, r *http.Request, _
 			return
 		}
 		json.NewEncoder(w).Encode(list)
+	}
+
+}
+
+// getMovie returns a handler for GET /movie/:id requests
+func getMovie(s listing.Service) func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		w.Header().Set("Content-Type", "application/json")
+		id, err := strconv.Atoi(p.ByName("id"))
+		if err != nil {
+			log.Println(err)
+			http.Error(w, http.StatusText(400), 400)
+			return
+		}
+		movie, err := s.GetMovie(id)
+		if err == listing.ErrNotFound {
+			http.Error(w, "The movie you requested does not exist.", http.StatusNotFound)
+			return
+		}
+		if err != nil {
+			log.Println(err)
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+		json.NewEncoder(w).Encode(movie)
 	}
 
 }
